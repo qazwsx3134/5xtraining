72be11f9c39eff971e6057e7b90f686003f1dfe2
Peter Tsai <qazwsx3134@gmail.com>
2020-02-10 21:47:06 +0800

    edit Webpack config and Eslint it

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,42 @@
 const path = require('path');
 
 
-
-
-module.exports = (env)=>{
-
+module.exports = () => {
   const isProduction = process.env.NODE_ENV === 'production';
 
-  return{
-  entry: ['babel-polyfill','./src/app.js'],
-  output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        'css-loader'
-      ]
-    },{
-      test: /\.(jpe?g|png|gif|svg)$/,
-      use: [
-        {
-          loader: 'url-loader',
-          options: { limit: 400000 }
-        },
-        'image-webpack-loader'
-      ]
-    }
-  ]
-  },
-  devtool: isProduction ? 'source-map' : 'inline-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    historyApiFallback: true
-  }
-}
+  return {
+    entry: ['babel-polyfill', './src/app.js'],
+    output: {
+      path: path.join(__dirname, 'public'),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      }, {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      }, {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: { limit: 400000 },
+          },
+          'image-webpack-loader',
+        ],
+      },
+      ],
+    },
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+    },
+  };
 };
